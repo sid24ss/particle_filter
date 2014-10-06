e796a089d8ba4e7ed003d8b376ac54a6e45ef0e7
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2014-10-06 06:09:43 -0400

    Butchered RobotState.h.

--- include/pf/RobotState.h
+++ include/pf/RobotState.h
@@ -5,23 +5,23 @@
 
 #include <pf/Constants.h>
 
-namespace pf {
+namespace pf{
     class RobotState {
     public:
-        RobotState() : state_(3, 0) { }
-        RobotState(std::vector<double> state) : state_(state) { }
+        RobotState();
+        RobotState(std::vector<double> state);
         // set and get methods for x
-        void x(double x) { state_[RobotDOF::X] = x; }
-        double x() const { return state_[RobotDOF::X]; }
+        void x(double x);
+        double x();
         // set and get methods for y
-        void y(double y) { state_[RobotDOF::Y] = y; }
-        double y() const { return state_[RobotDOF::Y]; }
+        void y(double y);
+        double y();
         // set and get methods for theta
-        void theta(double theta) { state_[RobotDOF::THETA] = theta; }
-        double theta() const { return state_[RobotDOF::THETA]; }
+        void theta(double theta);
+        double theta();
         // set and get methods for pose
         void state(std::vector<double> state);
-        std::vector<double> state() const { return state_; }
+        std::vector<double> state();
     private:
         std::vector<double> state_;
     };
