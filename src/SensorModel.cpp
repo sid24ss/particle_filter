31dc91735f12b9c8f9a0d2701f3168569f88e761
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2014-10-07 00:57:02 -0400

    SensorModel calculates weights. Code builds.

--- src/SensorModel.cpp
+++ src/SensorModel.cpp
@@ -5,18 +5,27 @@
 
 using namespace pf;
 
-
 SensorModel::SensorModel(MapPtr mapptr) :
     map_(mapptr)
-    // variance_scaling_(0.01),
-    // max_range_(800.0),
-    // min_range(0.0)
 {}
 
-double SensorModel::probMeasurementAtPose(double measurement, RobotState state)
+double SensorModel::calculateWeight(std::vector<double> ranges, RobotState state)
+{
+    filterRanges(ranges);
+    ranges = undersampleData(ranges);
+    bearings = SensorModelParams::getBearings();
+    bearings = undersampleData(bearings);
+    double prob = 1;
+    for (std::size_t i = 0; i < ranges.size(); ++i) {
+        prob = prob*probMeasurementAtPose(ranges[i], bearings[i], state);
+    }
+    return prob;
+}
+
+double SensorModel::probMeasurementAtPose(double measurement, double bearing, RobotState state)
 {
     // TODO: get nominal range by raycasting
-    double nominal_range = 0.5; 
+    double nominal_range = map_->getNominalReading(state, bearing);
     double prob = probMeasurement(measurement, nominal_range);
     return prob;
 }
@@ -26,20 +35,37 @@ double SensorModel::probMeasurement(double measurement, double nominal_range)
     double mean = nominal_range;
     double std = SensorModelParams::variance_scaling*nominal_range;
     double prob;
-    
+
     if (mean < SensorModelParams::max_range) {
-        // TODO: is direct computation faster than using Boost distributions?
-        boost::math::normal_distribution<> distribution(mean, std);
-        if(measurement < SensorModelParams::min_range || measurement > SensorModelParams::max_range) {
-            prob = 0;
-        } else {
+            boost::math::normal_distribution<> distribution(mean, std);
             double normalizer = 
                 boost::math::cdf(distribution, SensorModelParams::max_range)
-              - boost::math::cdf(distribution, SensorModelParams::min_range);
+                - boost::math::cdf(distribution, SensorModelParams::min_range);
             prob = boost::math::pdf(distribution, measurement)/normalizer;
-        }
-    } else {
-        // TODO: what is this behavior?
+    }
+    else {
+        if (measurement == SensorModelParams::max_range)
+            prob = 1;
+        else
+            prob = 0;
     }
     return prob;
 }
+
+void SensorModel::filterRanges(std::vector<double>& ranges)
+{
+    for (std::size_t i = 0; i < ranges.size(); ++i) {
+        if (ranges[i] > SensorModelParams::max_range)
+            ranges[i] = SensorModelParams::max_range;
+    }
+}
+
+std::vector<double> SensorModel::undersampleData(std::vector<double> data)
+{
+    std::vector<double> undersampled_data;
+    for (std::size_t i =0; i < data.size(); i = i + SensorModelParams::skip) {
+        undersampled_data.push_back(data[i]);
+    }
+    return undersampled_data;
+}
+
